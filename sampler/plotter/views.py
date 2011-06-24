f3b1592be8fb29601c1ebeb7b7302f53ce892b21
pr0ton <pratiktandel99@gmail.com>
2011-06-24 16:35:03 -0700

    Removed Simplegraph and added Highcharts for showing graphs

--- sampler/plotter/views.py
+++ sampler/plotter/views.py
@@ -5,6 +5,7 @@
 from django.shortcuts import *
 from django.utils import simplejson
 import json
+import math
 
 def index(request):
   return render_to_response('index.html')
@@ -12,25 +13,24 @@ def index(request):
 def query(request):
   try:
     params = request.REQUEST
-    lo, hi = int(params.get("lo")), int(params.get("hi"))
+    lo, hi, gran = int(params.get("lo")), int(params.get("hi")), int(params.get("granularity"))
     f = open('/home/pratik/grp/sampler/plotter/input.txt', 'r')
-    LIMIT, i = 20, 0
-    
+    LIMIT, i = 20*gran, 0
     labels, points = [], []
     for n in f:
       if lo <= i and i <= hi:
         points.append( n[:-1] )
         labels.append( i )
       i += 1
-    
-    _points = []
-    _labels = []
-    step = (hi - lo) / LIMIT
-    for z in xrange(0, len(points), step):
-       _points.append( points[z] )
-       _labels.append( labels[z] )
-    labels = _labels
-    points = _points
+    if hi-lo > LIMIT:
+      _points = []
+      _labels = []
+      step = (hi - lo) / LIMIT
+      for z in xrange(0, len(points), step):
+         _points.append( points[z] )
+         _labels.append( labels[z] )
+      labels = _labels
+      points = _points
     req = {"labels" : labels, "points" : points}
     return HttpResponse(simplejson.dumps(req), mimetype="application/json")
   except:
